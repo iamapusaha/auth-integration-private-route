e69283bde258e1ac3432a781a933eddfbcb21c5c
iamapusaha <apusaha392@gmail.com>
2023-10-04 00:01:46 +0600

    add lodder

--- src/components/Routes/PrivateRoute.jsx
+++ src/components/Routes/PrivateRoute.jsx
@@ -5,10 +5,19 @@ import { AuthContext } from '../../providers/AuthProvider';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
-    const { user } = useContext(AuthContext)
+    const { user, lodder } = useContext(AuthContext)
+    if (lodder) {
+        return <>
+            <span className="loading loading-spinner loading-xs"></span>
+            <span className="loading loading-spinner loading-sm"></span>
+            <span className="loading loading-spinner loading-md"></span>
+            <span className="loading loading-spinner loading-lg"></span>
+        </>
+    }
     if (user) {
         return children;
     }
+
     return <Navigate to='/login'></Navigate>
 };
 
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -5,21 +5,26 @@ import auth from "../Firebase/firebase.config";
 export const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
-
+    const [lodder, setLodder] = useState(true)
 
     const createUser = (email, password) => {
+        setLodder(true)
         return createUserWithEmailAndPassword(auth, email, password)
+
     }
     const signInUser = (email, password) => {
+        setLodder(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
     const logOut = () => {
+        setLodder(true)
         return signOut(auth)
     }
-    const authinfo = { user, createUser, signInUser, logOut }
+    const authinfo = { user, createUser, signInUser, logOut, lodder }
     useEffect(() => {
         const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
+            setLodder(false)
             console.log(' from useEffect', currentUser);
         })
         return () => {
